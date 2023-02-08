2e7fa77a7b7fb7b4efaeee92abffa84f7d1820d0
askeladd654 <123724414+askeladd654@users.noreply.github.com>
2023-02-08 09:28:49 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,8 +1,8 @@
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
 from os import path as ospath, environ, execl as osexecl
 from subprocess import run as srun
-from dotenv import load_dotenv
 from requests import get as rget
+from dotenv import load_dotenv
 from sys import executable
 
 if ospath.exists('log.txt'):
@@ -31,28 +31,37 @@ except:
 
 load_dotenv('config.env', override=True)
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
-if len(UPSTREAM_REPO) == 0:
-   UPSTREAM_REPO = 'https://github.com/askeladd654/jmdkh-nais'
-
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
-if len(UPSTREAM_BRANCH) == 0:
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
+try:
+    if len(UPSTREAM_REPO) == 0:
+       raise TypeError
+except:
+    UPSTREAM_REPO = "https://github.com/askeladd654/jmdkh-nais"
+try:
+    if len(UPSTREAM_BRANCH) == 0:
+       raise TypeError
+except:
     UPSTREAM_BRANCH = 'master'
 
-if UPSTREAM_REPO is not None:
-    if ospath.exists('.git'):
-        srun(["rm", "-rf", ".git"])
+if ospath.exists('.git'):
+    srun(["rm", "-rf", ".git"])
 
-    update = srun([f"git init -q \
-                     && git config --global user.email jmdkh007@gmail.com \
-                     && git config --global user.name jmdkh \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+update = srun([f"git init -q \
+                 && git config --global user.email doc.adhikari@gmail.com \
+                 && git config --global user.name Karan \
+                 && git add . \
+                 && git commit -sm update -q \
+                 && git remote add origin {UPSTREAM_REPO} \
+                 && git fetch origin -q \
+                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
-    if update.returncode == 0:
-        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
-    else:
-        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+if update.returncode == 0:
+    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    log_info(f'Upstream Repo: {UPSTREAM_REPO}')
+    log_info(f'Upstream Branch: {UPSTREAM_BRANCH}')
+else:
+    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+    log_info(f'Entered Upstream Repo: {UPSTREAM_REPO}')
+    log_info(f'Entered Upstream Branch: {UPSTREAM_BRANCH}')
+
